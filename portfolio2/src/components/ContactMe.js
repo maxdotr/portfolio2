101afb38942fe09ed8f1bd13004b471cd0bd51bb
mxxwwl <maxdotrodgers@gmail.com>
2022-03-29 13:56:14 -0600

    Styling and adding contact cards

--- portfolio2/src/components/ContactMe.js
+++ portfolio2/src/components/ContactMe.js
@@ -7,7 +7,7 @@ import { Outlet, Link } from 'react-router-dom';
 
 const ContactMe = () => {
   return (
-    <Container className={classes.contactContainer}>
+    <Container className={`p-5 ${classes.contactContainer}`}>
       <h1>Like what you see?</h1>
       <br />
       <br />
@@ -16,6 +16,48 @@ const ContactMe = () => {
           <b>Lets get to work.</b>
         </i>
       </h3>
+      <Container>
+        <Row className={`p-5 mx-auto`}>
+          <Col className={`col-lg-6 col-12 mx-auto`}>
+            <Container className={`m-5 p-2`}>
+              <Card>
+                <h5 className={`card-title ${classes.cardTitle} m-1`}>
+                  Call/Text
+                </h5>
+                <b className='mx-auto'>
+                  <p className={`card-text ${classes.cardText} mx-auto`}>
+                    1 (916) 280-4533
+                  </p>
+                </b>
+                <br />
+                <br />
+                <Button variant='outline-dark' className='btn-outline-dark m-5'>
+                  Copy to clipboard!
+                </Button>
+              </Card>
+            </Container>
+          </Col>
+          <Col className={`col-lg-6 col-12 mx-auto`}>
+            <Container className={`m-5 p-2`}>
+              <Card>
+                <h5 className={`card-title ${classes.cardTitle} m-1`}>
+                  Email me!
+                </h5>
+                <b className='mx-auto'>
+                  <p className={`card-text ${classes.cardText} mx-auto m-1`}>
+                    maxdotrodgers@gmail.com
+                  </p>
+                </b>
+                <br />
+                <br />
+                <Button variant='outline-dark' className='btn-outline-dark m-5'>
+                  Copy to clipboard!
+                </Button>
+              </Card>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 };
